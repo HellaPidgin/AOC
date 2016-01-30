07a179be5a6c6632994f71552bb6723b4821e475
Sam O <sam@samokoro.com>
2016-01-30 17:14:10 -0500

    Environment + First View

---
+++ App/Components/home.js
@@ -0,0 +1,25 @@
+import React, {
+  AppRegistry,
+  Component,
+  StyleSheet,
+  Text,
+  Image,
+  View
+} from 'react-native';
+
+class Home extends React.Component {
+        constructor(props) {
+         super(props);
+
+     }
+    render(){
+    return(
+        <View>
+            <Text>HI</Text>
+        </View>
+    )
+    }
+}
+
+
+module.exports = Home;
---
+++ index.ios.js
@@ -0,0 +1,67 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ */
+'use strict';
+import React, {
+  AppRegistry,
+  Component,
+  StyleSheet,
+  Text,
+      NavigatorIOS,
+  Image,
+  View
+} from 'react-native';
+
+var Home =  require("./App/Components/home");
+
+var MOCKED_MOVIES_DATA = [
+  {title: 'Title', year: '2015', posters: {thumbnail: 'http://i.imgur.com/UePbdph.jpg'}},
+];
+
+
+class AoC extends Component {
+    constructor(props) {
+    super(props);
+   
+
+  }
+   render(){
+
+     return (
+    <NavigatorIOS
+      initialRoute={{
+        component: Home,
+        title: 'My View Title',
+        passProps: { myProp: 'foo' },
+      }}
+    />
+  );
+   
+    
+}
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#F5FCFF',
+  },
+  welcome: {
+    fontSize: 20,
+    textAlign: 'center',
+    margin: 10,
+  },
+  instructions: {
+    textAlign: 'center',
+    color: '#333333',
+    marginBottom: 5,
+  },thumbnail: {
+    width: 53,
+    height: 81,
+  },
+});
+
+AppRegistry.registerComponent('AoC', () => AoC);
