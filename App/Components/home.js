be2ffc5f0bd1b73afe28f8e43a592decd6a6ddb1
Sam O <sam@samokoro.com>
2016-01-30 21:18:33 -0500

    Full podcast view

--- App/Components/home.js
+++ App/Components/home.js
@@ -44,12 +44,29 @@ class Home extends React.Component {
             </View>
             </View>
         <View style={styles.feed}>
-            <Text style={styles.sectTitle}>The Feed</Text>
-            <ScrollView style={styles.scrollV}>
-                <View><Image source={require('image!rec')} resizeMode="contain" style={styles.bigImage} /><Text>HI</Text></View>
+            <Text style={styles.sectTitle}>THE PODCAST FEED</Text>
+            <ScrollView  contentContainerStyle={styles.row} horizontal={true}
+             automaticallyAdjustContentInsets={false} vertical={false}>
+                <View><Image source={require('image!linda')} resizeMode="contain" style={styles.boxImage} /> 
+                    <Image source={require('image!44')} resizeMode="contain" style={styles.boxImage} />
+                    </View>
+                    <View><Image source={require('image!kamp')} resizeMode="contain" style={styles.boxImage} /> 
+                    <Image source={require('image!47')} resizeMode="contain" style={styles.boxImage} />
+                    </View>
+                    <View><Image source={require('image!maria')} resizeMode="contain" style={styles.boxImage} /> 
+                    <Image source={require('image!bill')} resizeMode="contain" style={styles.boxImage} />
+                    </View>
+                    <View><Image source={require('image!44')} resizeMode="contain" style={styles.boxImage} /> 
+                    <Image source={require('image!47')} resizeMode="contain" style={styles.boxImage} />
+                    </View>
+                  
                 </ScrollView>
+                    <View style={styles.divider}/>
             </View>
-        <View style={styles.sponsore}></View>
+        <View style={styles.sponsore}>
+            <Text style={styles.sponsoreTitle}>SPONSOR</Text>
+            <Image source={require('image!meundies')} resizeMode="contain" style={{width:350,height:200, borderRadius:10,marginTop:5, alignSelf:'center'}} />
+            </View>
         <View style={styles.favs}></View>
         </ScrollView>
         </View>
@@ -59,86 +76,118 @@ class Home extends React.Component {
 
 const styles = StyleSheet.create({
   container: {
-        flex: 1,
+         flex: 1,
 
-        flexDirection: 'column',
-        backgroundColor: '#FFF',
-    },
-    caption:{
-        width:120,
-        textAlign:'center',
-       fontSize:11,
-        color:'#555',
-        justifyContent:'center',
-    marginBottom:5,
-        marginTop:-5
-    },
-    col: {
-        flex: 1,
-        flexDirection: 'column',
- 
-    },
-    body:{
-    flex:9
-    },
-    row: {
-        flex: 1,
-       justifyContent:'flex-start'
- 
-    },
-    scrollV: {
-        margin: 10,
-    },
-    bigImage: {
-       height: 120,
-        width:100,
-        padding:0,
-        margin:15,
-        marginLeft:10,
-    },
-    recomends: {
-        flex: 4,
+         flexDirection: 'column',
+         backgroundColor: '#FFF',
+     },
+ sponsoreTitle:{
+ color:'#e5884f',
+     margin:10,
+     marginTop:0,
+     marginLeft:0
+ },
+     caption: {
+         width: 100,
+         textAlign: 'center',
+         fontSize: 11,
+         color: '#555',
+         justifyContent: 'center',
+         marginBottom: 5,
 
-        flexDirection: 'column',
+         marginTop: -5
+     },
+     col: {
+         flex: 1,
+         flexDirection: 'column',
+
+     },
+     body: {
+         flex: 9
+     },
+     row: {
+         flex: 1,
+         justifyContent: 'flex-start'
+
+     },
+     scrollV: {
+         margin: 10,
+     }, feed: {
+         margin: 10,
+          marginTop: -5,
+         
+     },
+    sponsore: {
+        flex:1,
+         margin: 15,
+        marginTop:0,
+         justifyContent:'center'
+         
+     },
+     bigImage: {
+         height: 120,
+         width: 100,
+         padding: 0,
+         margin: 15,
+         marginLeft: 0,
+         marginRight: 10
+     },
+     boxImage: {
+         height: 100,
+         width: 100,
+         padding: 0,
+         margin: 3,
+         marginLeft: 0,
+         marginRight: 10,
+         marginTop:20
+     },
+     recomends: {
+         flex: 4,
+         marginTop: -10,
+         flexDirection: 'column',
+
+     },
+     nav: {
+         flex: 1,
+         backgroundColor: '#202224',
+         justifyContent: 'center',
+         flexDirection: 'row',
+         marginTop: 20,
+         alignItems: 'center',
+
+     }, white: {
+         color: "#fff",
+         fontSize: 20,
+
+     },
+     welcome: {
+         fontSize: 20,
+         textAlign: 'center',
+         margin: 10,
+     },
+     divider: {
+         flex: 1,
+         marginTop: 15,
+         marginBottom: 15,
+         height: 2,
+         backgroundColor: '#f1f1f1',
+     }, sectTitle: {
+         fontSize: 14,
        
-    },
-    nav: {
-        flex: 1,
-        backgroundColor: '#202224',
-        justifyContent: 'center',
-        flexDirection:'row',
-        marginTop:20,
-        alignItems: 'center',
-
-    }, white: {
-        color: "#fff",
-        fontSize: 20,
-
-    },
-    welcome: {
-        fontSize: 20,
-        textAlign: 'center',
-        margin: 10,
-    }, divider: {
-       flex:1,
-        marginTop:15,
-        marginBottom:15,
-        height:1,
-        backgroundColor:'#f1f1f1',
-    },
-    icon: {
-        width: 14,
-        margin:5,
-    },
-    instructions: {
-        textAlign: 'center',
-        color: '#333333',
-        marginBottom: 5,
-    }, thumbnail: {
-        width: 53,
-        height: 81,
-    },
-});
+     },
+     icon: {
+         width: 14,
+         margin: 5,
+     },
+     instructions: {
+         textAlign: 'center',
+         color: '#333333',
+         marginBottom: 5,
+     }, thumbnail: {
+         width: 53,
+         height: 81,
+     },
+ });
 
 
 module.exports = Home;
